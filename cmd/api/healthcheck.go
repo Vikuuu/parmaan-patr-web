10d8e7c10ae4fc4f26755e297bd2269a402dd86a
Vikuuu <adivik672@gmail.com>
2026-04-26 19:26:20 +0530

    feat: Use json response for healtcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,7 +8,13 @@ import (
 // Health check handler writes a plain-text response with information
 // about application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a fixed-format JSON response from string.
+	// We are using string literal so that we can include double-quote
+	// characters in the JSON without needing to escape them.
+	// We also use the %q verb to wrap the interpolated values in double-quotes.
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
